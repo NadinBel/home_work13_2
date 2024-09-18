428676fa9750a8eeebf07e238d6b030062948797
NadinBel <143278036+NadinBel@users.noreply.github.com>
2024-09-18 13:42:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from aiogram import Bot, Dispatcher, types, executor
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+@dp.message_handler(text=['/start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+
+@dp.message_handler()
+async def all_massages(message):
+    print('Введите команду /start, чтобы начать общение')
